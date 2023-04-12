ea5ae7e6f8bdacd90127ad9cb4b37a6d83245412
Matheus Augusto <auguzsto@gmail.com>
2023-04-12 07:25:09 -0300

    permission routes

--- src/main/java/io/backend/security/SecurityConfig.java
+++ src/main/java/io/backend/security/SecurityConfig.java
@@ -19,6 +19,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests().
+        requestMatchers("POST", "/user").permitAll().
+        requestMatchers("GET", "/product").permitAll().
         anyRequest().
         authenticated().
         and().
